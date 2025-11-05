62769e0c06d564b393e66f19510485530c4ffbd0
Vitor Henrique <vitordeoliveira1127@gmail.com>
2025-11-05 13:14:49 -0300

    estrutura main produto

--- include/produto.h
+++ include/produto.h
@@ -1,7 +1,9 @@
 #ifndef PRODUTO_H
 #define PRODUTO_H
+#include <ncurses> 
 #define MAX_PRODUTO 100 //para o caso de chegar no limite o estoque
 
+//Estrutura do produto
 typedef struct {
     int id; // Um número de identificação único para cada produto.
     char descricao[100]; //A descrição do produto.
@@ -9,18 +11,37 @@ typedef struct {
     int estoque; // A quantidade disponível do produto em estoque.
 } Produto;
 
+//Estrutura do limite do estoque 
 typedef struct{
     Produto lista[MAX_PRODUTOS];
     int contador; //para controlar a quantidade de produtos no vetor 
 } ListaProdutos
 
-int AnalisarProduto(int codigo);
-void CadastrarProduto( int codigo);
-void ListarProdutos();
-void ConsultarProdutos(int codigo);
-void RemoverProduto();
+//Estrutura da descrição RASCUNHO
+typedef struct{
 
+    char nome[50];
+    int codigos; 
+    char categoria[50];
+
+}
+
+//Menu produtos 
+void MostrarMenuProduto();
+
+
+int AnalisarProduto(int codigo);
+void CadastrarProduto( ListaProduto *listaP, int codigo);
+void ListarProdutos(ListaProduto *listaP);
+void EditarProduto(ListaProduto *listaP, int codigo);
+void ConsultarProdutos(ListaProduto *listaP, int codigo);
+void RemoverProduto( ListaProduto *listaP, int codigo);
+
+//Dúvida essa *lista tem que ser com o nome diferente para todos? #########
 
 //As funções de salvar ou carregar produto no CSV
+void salvarProdutosCSV();
+void carregarProdutosCSV();
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,5 +2,69 @@
 
 int main() {
     printf("Projeto SCP iniciado com sucesso!\n");
-    return 0;
+
+    int codigo;
+
+    struct produto *p;
+    p = malloc(sizeof(struct produto));
+
+
+Produto novo_produto;
+
+// Cadastro do produto
+printf("Digite o codigo do produto: ");
+scanf ("%d", &novo_id);
+//Verificação
+//Chamando a função para analisar 
+AnalisarProduto(novo_id);
+//Se retornar 1 o codigo já existe
+printf("ERRO: O código já está cadastrado no sistema, tente outro: ");
+//Retornar para o menu produto
+MostrarMenuProduto();
+
+// Se não retornou 1 o codigo não existe e está liberado para cadastro
+//chamando a função de cadastro
+cadastro(novo_id);
+
+//retorna botão "cadastrar" clicado
+
+printf("O produto %d foi cadastrado com sucesso!\n", novo_produto.id); //confirmar exatamente que o produto x foi cadastrado
+
+//Botão sair 
+MostrarMenuPrincipal();
+
+
+//Listamento dos produtos
+ListarProdutos();
+
+//Botão sair 
+MostrarMenuPrincipal();
+
+
+//Consultar um produto (apenas leitura)
+
+printf("Digite o código do produto: "); //Não deveria ser o nome? 
+scanf("%d", &codigo);
+ConsultarProdutos(codigo);
+
+//Botão sair 
+MostrarMenuPrincipal();
+
+//Remover produto 
+printf("Digite o código do produto: "); 
+scanf("%d", &codigo);
+RemoverProduto(codigo);
+
+//Botão sair 
+MostrarMenuPrincipal();
+
+
+return 0;
+
+
+
+
+
+
 }
+
