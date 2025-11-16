e372f63156f2ceba5eb8f478b3afaa375d8b728b
Vitor Henrique <vitordeoliveira1127@gmail.com>
2025-11-16 16:05:24 -0300

    interface produto

--- src/interface.c
+++ src/interface.c
@@ -75,3 +75,75 @@ void MenuPrincipal(){
     }
 
 }
+void MostrarMenuProduto() {
+
+    //variaveis 
+    int i,c; 
+    initscr(); //para iniciar a tela e o PDCurses 
+    keypad(stdscr, TRUE); // para teclas especiais 
+    int total_opcoes = 5;
+    int selecao_atual = 0;
+
+
+    //vetor de opcoes 
+
+    char *opcoes[] ={
+
+        "Cadastrar";
+        "Listar";
+        "Consultar";
+        "Editar";
+        "Remover";
+
+    }
+
+    //estrutura de opções 
+
+    do{
+        clear();
+
+        mvprintw(2, 3 "    MODULO PRODUTOS   ");
+
+        for(i=0;i<total_opcoes;i++){
+            
+            if(i == selecao_atual) {
+
+                attron(A_REVERSE);
+                mvprintw(4 + i, 4 " > %s", opcoes[i]);
+                attroff(A_REVERSE);
+            } else {
+                mvprintw(4 + i, 4 " %s", opcoes[i]);
+            }
+
+        }
+
+    c=getch; //Capiturei a tecla clicada
+    
+    //Movimentos, igual do Menu Principal 
+    switch(c) { 
+       case KEY_UP: // pressionando a tecla cima, movimenta para cima 
+       selecao_atual = (selecao_atual == 0) ? total_opcoes -1 : selecao_atual -1;
+       break;
+       case KEY_DOWN: //pressioando a tecla baixo, movimenta para baixo
+       selecao_atual = (selecao_atual == total_opcoes -1) ? 0 : selecao_atual +1;
+       break; 
+       case 10: 
+       break; 
+    } while(c != 10 || selecao_atual != 4);
+    
+    if( c== 10){
+        clear();
+        refresh();
+
+        //chamando a função selecionada
+        switch(selecao_atual){ 
+            case 0: Cadastrar; break;
+            case 1: Listar; break;
+            case 2: Consultar; break;
+            case 3: Editar; break;
+            case 4: Remover; break; 
+            case 5: break; //voltar ao menu principal
+        }
+    }
+
+}
