f8ec627af6e9c340dc57b107f88f45589c0b48ad
Nycolas <165157675+Nyck02@users.noreply.github.com>
2025-11-19 01:06:08 -0300

    foi Adcionado funções parar encontrar um pedido
    Refactor pedido.c to include necessary headers and implement functions for finding orders by ID and client ID.

--- src/pedido.c
+++ src/pedido.c
@@ -1,9 +1,27 @@
-#include <ncurses.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include pedido.h 
-#include "cliente.h"
-#include produto.h 
 
+//Lista global de pedidos
+Pedido listaPedidos[MAX_PEDIDOS];
+int qtdPedidos = 0; 
 
+//função para achar o pedido pelo ID
+int encontrarPedidoPorId(int id) {
+  for(int i = 0; i < qtdPedidos; i++){
+    if(listaPedidos[i].id == id){
+      return i;
+    }
+  }
+return -1 //quando não encontra pedido 
+  } 
+//Função para encontrar um pedido pelo clienteId
+int encontrarPedidoPorClienteId(int clienteId) {
+  for(int i= 0; i < qtdPedidos. i++) {
+   if(listaPedidos[i].clienteId == clienteId) {
+       return i;
+   }
+  }
+    return -1;
+}
