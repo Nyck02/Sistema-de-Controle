f0e8405b414d7212ea99014762dd171a064ac2f2
vitor henrique ferreira de oliveira <vitor@DESKTOP-NM5IGGD.localdomain>
2025-12-03 18:20:03 -0300

    alteracoes

--- Sistema-de-Controle/src/main.c
+++ Sistema-de-Controle/src/main.c
@@ -3,20 +3,18 @@
 #include "produto.h"
 
 int main() {
-    // 1. INICIALIZAÇÃO OBRIGATÓRIA (O "Motor de Partida")
+    // Partida
     initscr();              // Liga a tela
-    cbreak();               // Leitura rápida de teclas
+    cbreak();               // Leitura rapida de teclas
     noecho();               // Não imprime o que digita
     curs_set(0);            // Esconde cursor
     keypad(stdscr, TRUE);   // Habilita setas
 
-    // Inicializa o banco de dados (Importante!)
+    // Inicializa
     InicializarProdutos(); 
 
-    // 2. CHAMA O SISTEMA
     MenuPrincipal();
 
-    // 3. DESLIGA TUDO
     endwin();
     return 0;
 }
